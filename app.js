a4e178e5d8dfb7c1e836378c09f8cac1713a7f0c
John Rees <john@bitsushi.com>
2012-09-10 10:59:43 +0100

    changes

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-// look at https://github.com/kuno/GeoIP
-
 var express = require('express');
+
+// look at https://github.com/kuno/GeoIP
+// var geoip = require('geoip-lite');
+// var ip = "207.97.227.239";
+// var geo = geoip.lookup(ip);
+// console.log(geo);
+
+var redis = require('redis');
+var db = redis.createClient();
+
 var app = express();
 
+// app.use(function(req, res, next){
+//   var ua = req.headers['user-agent'];
+// });
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  db.set('online', 'aa');
+  var a = db.get('online');
+  res.send(a);
 });
 
 app.listen(3000);
